9347145482b331242ce8373c892967ad743c174b
Bdmnss <lukabadali123@gmail.com>
2024-06-08 21:04:52 +0400

    giving rate useState from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Rate from "./Components/Rate/Rate";
+import { useState } from "react";
 
 function App() {
+  const [rate, setRate] = useState(0);
   return (
     <>
-      <Rate />
+      <Rate rate={rate} setRate={setRate} />
     </>
   );
 }
